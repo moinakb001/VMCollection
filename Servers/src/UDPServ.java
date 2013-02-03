cddfa81b4e9ab8d070b4c835f7e42403aeace3d6
themodder666 <mb.totaldomination@gmail.com>
2013-02-03 09:15:44 -0800

    Getting started on udp

--- Servers/src/UDPServ.java
+++ Servers/src/UDPServ.java
@@ -1,4 +1,5 @@
-
+import java.io.*;
+import java.net.*;
 public class UDPServ {
 
 	/**
@@ -6,6 +7,16 @@ public class UDPServ {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+DatagramSocket s=new DatagramSocket();
+byte[] recbytes=new byte[1024*50];
+byte[] send=new byte[1024*50];
+while(true){
+DatagramPacket p=new DatagramPacket(recbytes,recbytes.length);
+s.recieve(p);
+System.out.println("Got :"+(new String(p.getData())));
+
+}
+
 
 	}
 
